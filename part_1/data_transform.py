dc2174bc8b36ec43dd4e969bc60a678082eadffb
Anvi Alex <epononanvialex@gmail.com>
2024-05-15 21:23:15 -0600

    Transform dataset for binary classification tests on IBM debater ArgQuality

--- part_1/data_transform.py
+++ part_1/data_transform.py
@@ -6,31 +6,48 @@ import numpy as np
 import pandas as pd 
 import os 
 
-raw = pd.read_csv("../data/arg14/Online-shopping-brings-more-harm-than-good-(PRO).tsv", sep="\t")
-cols = ["label", "a1", "a2"]
-print(raw.info())
+# Get the list of tsv files from folders to read 
 
+def get_files(folder_path):
+    "Get the path of a folder containing files and return a list of the file names."
+    return os.listdir(folder_path)
+
+previous_loc = ".."
+root_folder = "./data/arg14/"
+files = get_files(os.path.join(previous_loc, root_folder))
+print(files)
+
+## get the .md files:
+
+# Loop through each file and construct the dataset 
 new_data = {'args': [], 'labels': []}
 
-mem = 1
-for i in range(len(raw)):
-    tmp_label = raw.iloc[i]["label"]
-    if mem == 1:
-        new_data['args'].append(raw.iloc[i][f"{tmp_label}"])
-        new_data['labels'].append(True)
-        mem = -1
-    elif mem == -1:
-        if tmp_label == 'a1':
-            new_data['args'].append(raw.iloc[i]['a2'])
-        elif tmp_label == 'a2':
-            new_data['args'].append(raw.iloc[i]['a1'])
-        
-        new_data['labels'].append(False)
-        mem = 1
-
-print(new_data)
+for file in files:
+    # open file
+    #with open(os.path.join(previous_loc, root_folder, file)) as f:
+    #raw = pd.read_csv("../data/arg14/Online-shopping-brings-more-harm-than-good-(PRO).tsv", sep="\t")
+    tmp_raw = pd.read_csv(f"{os.path.join(previous_loc, root_folder, file)}", sep="\t")
+    #cols = ["label", "a1", "a2"]
 
 
+    mem = 1
+    for i in range(len(tmp_raw)):
+        tmp_label = tmp_raw.iloc[i]["label"]
+        if mem == 1:
+            new_data['args'].append(tmp_raw.iloc[i][f"{tmp_label}"])
+            new_data['labels'].append(True)
+            mem = -1
+        elif mem == -1:
+            if tmp_label == 'a1':
+                new_data['args'].append(tmp_raw.iloc[i]['a2'])
+            elif tmp_label == 'a2':
+                new_data['args'].append(tmp_raw.iloc[i]['a1'])
+            
+            new_data['labels'].append(False)
+            mem = 1
+
+new_data = pd.DataFrame(new_data)
+new_data.to_csv("transformed_data.tsv", sep="\t", index=False)
 
 
 
