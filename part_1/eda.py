a4c0dc87b257bd4259162c0e100193c9c2eb9e56
Anvi Alex <epononanvialex@gmail.com>
2024-05-27 13:43:28 -0600

    extract ngrams for relevant args only

--- part_1/eda.py
+++ part_1/eda.py
@@ -10,6 +10,7 @@ import glob
 import os
 from sklearn.feature_extraction.text import CountVectorizer
 import operator
+from collections import Counter
 
 # load dataset 
 raw = pd.read_csv("../data/arguments.csv")
@@ -116,45 +117,33 @@ plt.show()
 #Word count on relevant_args to identify repeated terms 
 # On relevant_args only (1,2,3 grams)
 
-vectorizer = CountVectorizer(analyzer="word", ngram_range=(1,1))
-vectorizer2 = CountVectorizer(analyzer="word", ngram_range=(2,2))
-vectorizer3 = CountVectorizer(analyzer="word", ngram_range=(3,3))
-
-count_1 = vectorizer.fit_transform(relevant_args["text"])
-unigram = vectorizer.get_feature_names_out()
-df = pd.DataFrame(count_1.toarray(), columns=unigram)
-#df.to_csv("test00.csv")
-count_2 = vectorizer2.fit_transform(relevant_args["text"])
-twograms = vectorizer2.get_feature_names_out()
-
-count_3 = vectorizer3.fit_transform(relevant_args["text"])
-trigrams = vectorizer3.get_feature_names_out()
-
 words = relevant_args["text"]
 words = list(words.to_dict().values())
-words = " ".join(words).split(" ")
+words = [p.split(" ") for p in words] 
+text_words = [" ".join(p) for p in words]
 
-vocab = {}
+# Initialize CountVectorizer with custom analyzer and n_grams from 1-3
+print("counting..")
+vectorizer = CountVectorizer(ngram_range=(1, 3))
 
-for w in twograms:
-    i = 0
-    for i in range(len(words) - 1):
-        tmp_w = " ".join([words[i], words[i+1]])
-        if w.lower() == tmp_w.lower():
-            if tmp_w.lower() in list(vocab.keys()):
-                vocab[tmp_w.lower()] += 1 
-            elif tmp_w.lower() not in list(vocab.keys()):
-                vocab[tmp_w.lower()] = 1
-    i +=1
-    print(i)
+# Fit and transform the corpus
+print("fitting..")
+count_matrix = vectorizer.fit_transform(text_words)
 
-vocab = sorted(vocab.items(), key=operator.itemgetter(1))
+# Get feature names
+feature_names = vectorizer.get_feature_names_out()
 
-#print(vocab)
+# Convert count matrix to DataFrame
+df = pd.DataFrame(count_matrix.toarray(), columns=feature_names)
+
+print("saving..")
+#df.to_csv("test_00.csv", index=False)
+
 
 """Notes on this short EDA: 
-    - The dataset is comprised of 437 samples in an organised text format (csv)
-    - out of the samples, 437 are considered by the annonators arguments while 57 as Non-arguments
-    - While we have 437 considered arguments, only 280 are considered relevants taking a threshold of >=3. So 157 samples are considered non-relevant by annotators. 
-    - We found in the relevants arguments that 185 arguments are logical, 193 are dialectical and 174 are rhetorical which makes the distribution of dimensions in relevant args balanced. 
+    - The dataset is comprised of 6k samples in an organised text format (csv)
+    - out of the samples, __ are considered by the annonators arguments while 57 as Non-arguments
+    - While we have 6k considered arguments, only ____ are considered relevants taking a threshold of >=3. So ___ samples are considered non-relevant by annotators. 
+    - We found in the relevants arguments that ___ arguments are logical, ___ are dialectical and ___ are rhetorical which makes the distribution of dimensions in relevant args balanced. 
     - Which means that for an argument to be logic, it has to be relevant at first glance with the conclusion or topic from the dataset."""
+
--- part_1/test.py
+++ part_1/test.py
@@ -4,3 +4,18 @@ import pandas as pd
 raw = pd.read_csv("./merged_data_with_text.csv")
 raw2 = pd.read_csv("./transformed_data2.tsv", sep='\t')
 print(raw["args"].value_counts(), raw2["labels"].value_counts())
+
+text = "I am going to school"
+text = text.split(" ")
+s = "going to"
+
+count = 0
+for i in range(len(text) - 1):
+    tmp_  = " ".join([text[i], text[i+1]])
+    print(tmp_)
+    if s == tmp_:
+        count += 1
+
+print(count)
+        
+
